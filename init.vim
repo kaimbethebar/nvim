6c388653b9660e5ffcc92f3be3ec84ba619a6a64
kaimbethebar <jesse.rowan@proton.me>
2024-08-10 17:28:44 -0500

    Converted init.vim ot init.lua

--- init.vim
+++
@@ -1,3 +0,0 @@
-lua require('plugins')
-lua require('set')
-lua require('remap')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,9 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   use { "catppuccin/nvim", as = "catppuccin" }
-
-  --  use { "nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'}}
 
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
